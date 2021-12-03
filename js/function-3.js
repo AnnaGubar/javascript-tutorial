d449b493b44b9048cf624b238c75f84ad88d2deb
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2021-12-03 18:21:18 +0200

    adds calculator

--- js/function-3.js
+++ js/function-3.js
@@ -4,3 +4,44 @@ function foo() {
 
 const x = foo();
 console.log(`User wrote ${x}`);
+const num1 = Number(prompt('Введи первое число'));
+const num2 = Number(prompt('Введи второе число'));
+const operation = prompt('Укажи математический оператор');
+
+const calculator = function (x, y, oper) {
+  if (oper === '*') {
+    return x * y;
+  }
+  if (oper === '-') {
+    return x - y;
+  }
+  if (oper === '+') {
+    return x + y;
+  }
+  if (oper === '/') {
+    return x / y;
+  }
+  return alert('Введены не правильные значения');
+};
+
+console.log('Результат:', calculator(num1, num2, operation));
+
+// const LOGIN = 'Anna';
+// const PAROL = 'parolL2458';
+
+// const valid = function () {
+//   const login = prompt('Введи логин');
+
+//   if (login === LOGIN) {
+//     const parol = prompt('Введи пароль');
+//     if (parol === PAROL) {
+//       alert('Все ОК!');
+//       return 'success';
+//     }
+//   }
+
+//   alert('Что-то не так');
+//   return 'error';
+// };
+
+// console.log(valid());
