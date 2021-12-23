2b2472e5910b96d75bfff198c7b4e71ed2f61a0a
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2021-12-23 21:25:40 +0200

    final

--- js/typeof.js
+++
@@ -1,9 +0,0 @@
- //*typeof x*
- 
-                       //true
-console.log(typeof isNaN('i'));  //boolean
-
-let x = prompt('gfjkfjk');
-console.log(typeof x);
-x = Number(x);
-console.log(typeof x);
